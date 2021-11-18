150ac521c8b300a11a2a661465cd164f673a2483
Jarom Diaz <jarom3429@gmail.com>
2021-11-18 09:18:28 -0700

    Initial Commit

---
+++ greed/game/casting/artifact.py
@@ -0,0 +1,36 @@
+from game.casting.actor import Actor
+from game.shared.point import Point
+import random
+
+# TODO: Implem(ent the Artifact class here. Don't forget to inherit from Actor!
+class Artifact(Actor):
+    def __init__(self):
+        super().__init__()
+        self._message = ""
+        self._points = 0
+
+    def get_points(self):
+        return self._points
+
+    def set_points(self):
+        if self._text == "*":
+            self._points += 100
+        else:
+            self._points -= 100
+
+    def get_message(self):
+        return self._message
+
+    def set_message(self, message):
+        self._message = message
+
+    def move(self):
+        """Move the artifacts down by a random amount between 1 and 10"""
+        x = self._position.get_x()
+        y = self._position.get_y()
+        if y >= 600:
+        #    y = 15
+            pass
+        else:
+            y += 15
+        self._position = Point(x,y)
